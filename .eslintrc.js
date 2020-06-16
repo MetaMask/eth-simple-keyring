7aabea820efc41affd22203fa9ed8c6a82c0229b
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-06-16 11:53:19 -0700

    Standardize repo (#42)
    - Add `.nvmrc` with `v10`
    - Add `CODEOWNERS`
    - `yarn import`
    - Add `eslint` and `@metamask/eslint-config`; lint
    - Update CircleCI config
      - Shadows recent library CircleCI configs
      - Removes `--ignore-scripts` flag from deps install script, as `scrypt` has required scripts
    - Update dev dependencies to resolve security vulnerability
    - Remove re-declaration of `decryptMessage` class method
      - The re-declaration was identical, so it didn't matter in practice
    
    No functional changes were made, and no production dependencies were changed. It's possible that some of the syntactic changes per our lint rules are breaking for older versions of Node, so we may want to consider a major version bump to follow this. It should not affect us in any way.

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  extends: [
+    '@metamask/eslint-config',
+    '@metamask/eslint-config/config/nodejs',
+  ],
+  plugins: [
+    'json',
+  ],
+  overrides: [{
+    files: [
+      '.eslintrc.js',
+    ],
+    parserOptions: {
+      sourceType: 'script',
+    },
+  }],
+  rules: {
+    'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+    'camelcase': ['error', {
+      'allow': [
+        'signTypedData_v1',
+        'signTypedData_v3',
+        'signTypedData_v4',
+      ],
+    }],
+  },
+}
--- sample.js
+++ sample.js
@@ -1,22 +1,22 @@
-var SimpleKeyring = require('./')
+const SimpleKeyring = require('.')
 
-var keyring = new SimpleKeyring()
+const keyring = new SimpleKeyring()
 
 let accounts = []
 keyring.addAccounts(37)
-.then((newAddresses) => {
-  accounts = newAddresses.map((address) => {
-    return { address }
+  .then((newAddresses) => {
+    accounts = newAddresses.map((address) => {
+      return { address }
+    })
+    return keyring.serialize()
   })
-  return keyring.serialize()
-})
-.then((privateKeys) => {
-  privateKeys.forEach((privateKey, index) => {
-    accounts[index].privateKey = privateKey
+  .then((privateKeys) => {
+    privateKeys.forEach((privateKey, index) => {
+      accounts[index].privateKey = privateKey
+    })
   })
-})
-.then(() => {
-  const jsonAccounts = JSON.stringify(accounts, null, 2)
-  console.log(jsonAccounts)
-})
+  .then(() => {
+    const jsonAccounts = JSON.stringify(accounts, null, 2)
+    console.log(jsonAccounts)
+  })
 

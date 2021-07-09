624b34d6f5fdc9003a9f6298b8df4cd9aa6d930b
Mark Stacey <markjstacey@gmail.com>
2021-07-08 21:33:23 -0230

    Remove `ethereumjs-wallet`
    The `ethereumjs-wallet` package was replaced by a couple of simple
    utility functions. The only thing we used the "Wallet" abstraction for
    was generating a private key and getting the address that corresponds
    to that private key.
    
    This should behave identically to how it did before, except now we have
    one less dependency. This is especially beneficial in this case because
    `ethereumjs-wallet` had transitive dependencies that were inconvenient
    for us (e.g. native dependencies and things that make LavaMoat
    unimpressed).
    
    Closes #21, #59

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const { EventEmitter } = require('events');
-const Wallet = require('ethereumjs-wallet').default;
 const ethUtil = require('ethereumjs-util');
+const randomBytes = require('randombytes');
 
 const type = 'Simple Key Pair';
 const sigUtil = require('eth-sig-util');
+
+function generateKey() {
+  const privateKey = randomBytes(32);
+  // I don't think this is possible, but this validation was here previously,
+  // so it has been preserved just in case.
+  // istanbul ignore next
+  if (!ethUtil.isValidPrivate(privateKey)) {
+    throw new Error(
+      'Private key does not satisfy the curve requirements (ie. it is invalid)',
+    );
+  }
+  return privateKey;
+}
 
 class SimpleKeyring extends EventEmitter {
   constructor(opts) {
@@ -14,32 +27,36 @@ class SimpleKeyring extends EventEmitter {
   }
 
   async serialize() {
-    return this._wallets.map((w) => w.getPrivateKey().toString('hex'));
+    return this._wallets.map(({ privateKey }) => privateKey.toString('hex'));
   }
 
   async deserialize(privateKeys = []) {
-    this._wallets = privateKeys.map((privateKey) => {
-      const stripped = ethUtil.stripHexPrefix(privateKey);
-      const buffer = Buffer.from(stripped, 'hex');
-      const wallet = Wallet.fromPrivateKey(buffer);
-      return wallet;
+    this._wallets = privateKeys.map((hexPrivateKey) => {
+      const strippedHexPrivateKey = ethUtil.stripHexPrefix(hexPrivateKey);
+      const privateKey = Buffer.from(strippedHexPrivateKey, 'hex');
+      const publicKey = ethUtil.privateToPublic(privateKey);
+      return { privateKey, publicKey };
     });
   }
 
   async addAccounts(n = 1) {
     const newWallets = [];
     for (let i = 0; i < n; i++) {
-      newWallets.push(Wallet.generate());
+      const privateKey = generateKey();
+      const publicKey = ethUtil.privateToPublic(privateKey);
+      newWallets.push({ privateKey, publicKey });
     }
     this._wallets = this._wallets.concat(newWallets);
-    const hexWallets = newWallets.map((w) =>
-      ethUtil.bufferToHex(w.getAddress()),
+    const hexWallets = newWallets.map(({ publicKey }) =>
+      ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)),
     );
     return hexWallets;
   }
 
   async getAccounts() {
-    return this._wallets.map((w) => ethUtil.bufferToHex(w.getAddress()));
+    return this._wallets.map(({ publicKey }) =>
+      ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)),
+    );
   }
 
   // tx is an instance of the ethereumjs-transaction class.
@@ -70,7 +87,7 @@ class SimpleKeyring extends EventEmitter {
   // For eth_decryptMessage:
   async decryptMessage(withAccount, encryptedData) {
     const wallet = this._getWalletForAccount(withAccount);
-    const privKey = ethUtil.stripHexPrefix(wallet.getPrivateKey());
+    const privKey = ethUtil.stripHexPrefix(wallet.privateKey);
     const sig = sigUtil.decrypt(encryptedData, privKey);
     return sig;
   }
@@ -122,8 +139,7 @@ class SimpleKeyring extends EventEmitter {
       throw new Error('Must specify address.');
     }
     const wallet = this._getWalletForAccount(address, opts);
-    const privKey = ethUtil.toBuffer(wallet.getPrivateKey());
-    return privKey;
+    return wallet.privateKey;
   }
 
   // returns an address specific to an app
@@ -135,7 +151,7 @@ class SimpleKeyring extends EventEmitter {
       withAppKeyOrigin: origin,
     });
     const appKeyAddress = sigUtil.normalize(
-      wallet.getAddress().toString('hex'),
+      ethUtil.publicToAddress(wallet.publicKey).toString('hex'),
     );
     return appKeyAddress;
   }
@@ -143,21 +159,24 @@ class SimpleKeyring extends EventEmitter {
   // exportAccount should return a hex-encoded private key:
   async exportAccount(address, opts = {}) {
     const wallet = this._getWalletForAccount(address, opts);
-    return wallet.getPrivateKey().toString('hex');
+    return wallet.privateKey.toString('hex');
   }
 
   removeAccount(address) {
     if (
       !this._wallets
-        .map((w) => ethUtil.bufferToHex(w.getAddress()).toLowerCase())
+        .map(({ publicKey }) =>
+          ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)).toLowerCase(),
+        )
         .includes(address.toLowerCase())
     ) {
       throw new Error(`Address ${address} not found in this keyring`);
     }
     this._wallets = this._wallets.filter(
-      (w) =>
-        ethUtil.bufferToHex(w.getAddress()).toLowerCase() !==
-        address.toLowerCase(),
+      ({ publicKey }) =>
+        ethUtil
+          .bufferToHex(ethUtil.publicToAddress(publicKey))
+          .toLowerCase() !== address.toLowerCase(),
     );
   }
 
@@ -167,18 +186,20 @@ class SimpleKeyring extends EventEmitter {
   _getWalletForAccount(account, opts = {}) {
     const address = sigUtil.normalize(account);
     let wallet = this._wallets.find(
-      (w) => ethUtil.bufferToHex(w.getAddress()) === address,
+      ({ publicKey }) =>
+        ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)) === address,
     );
     if (!wallet) {
       throw new Error('Simple Keyring - Unable to find matching address.');
     }
 
     if (opts.withAppKeyOrigin) {
-      const privKey = wallet.getPrivateKey();
+      const { privateKey } = wallet;
       const appKeyOriginBuffer = Buffer.from(opts.withAppKeyOrigin, 'utf8');
-      const appKeyBuffer = Buffer.concat([privKey, appKeyOriginBuffer]);
-      const appKeyPrivKey = ethUtil.keccak(appKeyBuffer, 256);
-      wallet = Wallet.fromPrivateKey(appKeyPrivKey);
+      const appKeyBuffer = Buffer.concat([privateKey, appKeyOriginBuffer]);
+      const appKeyPrivateKey = ethUtil.keccak(appKeyBuffer, 256);
+      const appKeyPublicKey = ethUtil.privateToPublic(appKeyPrivateKey);
+      wallet = { privateKey: appKeyPrivateKey, publicKey: appKeyPublicKey };
     }
 
     return wallet;

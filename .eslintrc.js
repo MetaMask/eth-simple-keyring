3b77f52db78934170bd58fc7ca65de3d7231370b
Howard Braham <howrad@gmail.com>
2023-03-22 00:14:58 -0700

    partial response to @mcmire review

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,30 @@
 module.exports = {
   root: true,
-  extends: ['@metamask/eslint-config', '@metamask/eslint-config-nodejs'],
+
+  extends: ['@metamask/eslint-config'],
 
   overrides: [
-    {
-      files: ['**/*.test.ts'],
-      extends: ['@metamask/eslint-config-jest'],
-      rules: {
-        'node/no-unpublished-require': 0,
-      },
-    },
     {
       files: ['*.ts'],
       extends: ['@metamask/eslint-config-typescript'],
     },
-  ],
 
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    {
+      files: ['*.js'],
+      parserOptions: {
+        sourceType: 'script',
       },
+      extends: ['@metamask/eslint-config-nodejs'],
     },
-    node: {
-      tryExtensions: ['.js', '.json', '.node', '.ts'],
+
+    {
+      files: ['*.test.ts', '*.test.js'],
+      extends: [
+        '@metamask/eslint-config-jest',
+        '@metamask/eslint-config-nodejs',
+      ],
     },
-  },
+  ],
 
   // This is necessary to run eslint on Windows and not get a thousand CRLF errors
   rules: { 'prettier/prettier': ['error', { endOfLine: 'auto' }] },
@@ -33,7 +32,8 @@ module.exports = {
   ignorePatterns: [
     '!.eslintrc.js',
     '!.prettierrc.js',
-    'dist/**/*',
-    'jest.config.ts',
+    'dist/',
+    'docs/',
+    '.yarn/',
   ],
 };
